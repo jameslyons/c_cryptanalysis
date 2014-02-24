620076318c0a24c871d5eb37b2f62687f6d006a9
James Lyons <james.lyons0@gmail.com>
2014-02-24 18:10:10 +1000

    Update scoreText.h

--- hill3/scoreText.h
+++ hill3/scoreText.h
@@ -1,5 +1,4 @@
 /* author: James Lyons
-Aug 2012
-this code is from http://www.practicalcryptography.com/cryptanalysis/breaking-machine-ciphers/cryptanalysis-enigma/
 */
 double scoreTextQgram(char *text,int len);
+double scoreTextBigram(char *text,int len);
